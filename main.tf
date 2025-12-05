efd7c596ae1cf52627987fe68d7513e91f76539f
SURENDAR M <surendarm2209@gmail.com>
2025-12-05 19:51:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -171,4 +171,5 @@ resource "aws_iam_role_policy_attachment" "kubernetes_node_group_role_policy" {
 
 resource "aws_iam_role_policy_attachment" "kubernetes_node_group_cni_policy" {
   role       = aws_iam_role.kubernetes_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_P_
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
