191a281866ceab3337a55c6febebd4bedb6abcb9
SURENDAR M <surendarm2209@gmail.com>
2025-12-06 18:00:25 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,11 @@
-output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+output "cluster_name" {
+  value = aws_eks_cluster.devopsshack.name
 }
 
-output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+output "node_group_name" {
+  value = aws_eks_node_group.devopsshack.node_group_name
 }
 
 output "vpc_id" {
   value = aws_vpc.devopsshack_vpc.id
-}
-
-output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
 }
